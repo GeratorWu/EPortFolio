2d28cb16ac52233cf3722d2b0be565a5a256b623
Patrick Wu <118174626+GeratorWu@users.noreply.github.com>
2023-11-13 17:17:49 +0100

    github

--- src/components/Competence.js
+++ src/components/Competence.js
@@ -17,35 +17,41 @@ function Competence() {
 
     return (
         <div className='competencebloc'>
-            <div className='competencegauche'>
-                <h1>Mes compétences</h1>
-                <div className='competencelogiciel'>
-                    <div>
-                        <h2>Développement logiciel</h2>
-                        <p onClick={() => afficheProjet(1)}>Java</p>
-                        <p onClick={() => afficheProjet(2)}>VBnet</p>
-                        <p onClick={() => afficheProjet(3)}>C</p>
-                        <p onClick={() => afficheProjet(4)}>C++</p>
-                        <p onClick={() => afficheProjet(99)}>Python</p>
+            <div className='competencehaut'>
+                <div className='competencegauche'>
+                    <h1>Mes compétences</h1>
+                    <div className='competencelogiciel'>
+                        <div>
+                            <h2>Développement logiciel</h2>
+                            <p onClick={() => afficheProjet(1)}>Java</p>
+                            <p onClick={() => afficheProjet(2)}>VBnet</p>
+                            <p onClick={() => afficheProjet(3)}>C</p>
+                            <p onClick={() => afficheProjet(4)}>C++</p>
+                            <p onClick={() => afficheProjet(99)}>Python</p>
+                        </div>
+                        <div><h2>Développement Web</h2>
+                            <p onClick={() => afficheProjet(6)}>React</p>
+                            <p onClick={() => afficheProjet(5)}>Angular</p>
+                            <p onClick={() => afficheProjet(7)}>HTML</p>
+                            <p onClick={() => afficheProjet(7)}>CSS</p>
+                            <p onClick={() => afficheProjet(8)}>PHP</p>
+                            <p onClick={() => afficheProjet(8)}>SQL</p>
+                            <p onClick={() => afficheProjet(7)}>JavaScript</p>
+                        </div>
                     </div>
-                    <div><h2>Développement Web</h2>
-                        <p onClick={() => afficheProjet(6)}>React</p>
-                        <p onClick={() => afficheProjet(5)}>Angular</p>
-                        <p onClick={() => afficheProjet(7)}>HTML</p>
-                        <p onClick={() => afficheProjet(7)}>CSS</p>
-                        <p onClick={() => afficheProjet(8)}>PHP</p>
-                        <p onClick={() => afficheProjet(8)}>SQL</p>
-                        <p onClick={() => afficheProjet(7)}>JavaScript</p>
+                </div>
+                <div className='competencedroite'>
+                    <h1>Mes projets</h1>
+                    <div className='competenceprojet'>
+                        <h2 className='competencelangage'>{langage}</h2>
+                        <h2 className='competencetitre'>{titre}</h2>
+                        <p>{description}</p>
                     </div>
                 </div>
             </div>
-            <div className='competencedroite'>
-                <h1>Mes projets</h1>
-                <div className='competenceprojet'>
-                    <h2 className='competencelangage'>{langage}</h2>
-                    <h2 className='competencetitre'>{titre}</h2>
-                    <p>{description}</p>
-                </div>
+            <div className='competencebas'>
+                <p>Tous mes projets sont disponibles via ce lien :</p>
+                <a href='https://github.com/GeratorWu' className='liengithub'>https://github.com/GeratorWu</a>
             </div>
         </div>
     )
