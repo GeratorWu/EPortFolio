2f5d1e2c871e1452f040921899aa253092eddaea
Patrick Wu <118174626+GeratorWu@users.noreply.github.com>
2024-04-16 11:13:45 +0200

    maj photo + texte

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -6,7 +6,7 @@ function Accueil(){
         <div className='accueilbloc'>
             <div className='accueilgauche'>
                 <h1>Bonjour, c'est Patrick Wu</h1>
-                <p>Un étudiant en troisième année de BUT informatique à la recherche d'un stage à partir du 4 Mars d'une durée minimale de 14 semaines.</p>
+                <p>Je suis à la recherche d’un contrat d’apprentissage en tant que Développeur full stack pour 3 ans (rythme : 3 jours entreprise / 2 jours formation ; disponibilité : septembre 2024).</p>
             </div>
         </div>
     )
